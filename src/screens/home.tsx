10023cc23492d53bfe19f29548ff6ed71d958dab
Flare <blastclever@gmail.com>
2024-07-30 18:50:44 +0300

    fix(HomeScreen): disable linting for useEffect empty deps array

--- src/screens/home.tsx
+++ src/screens/home.tsx
@@ -28,6 +28,8 @@ export default function HomeScreen() {
 
   useEffect(() => {
     handleDataFetching();
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   async function handleDataFetching() {
